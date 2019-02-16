bcb2d5cb13e5849c7791b340287f107c261041ac
Manakov, Pavel <Pavel.Manakov@nexign-systems.com>
2019-02-16 13:33:28 +0700

    lab1 powershell

---
+++ lab1.ps1
@@ -0,0 +1,9 @@
+﻿function getName($len){
+    -join (1..$len | % { Get-Random -InputObject ("afgtdUHS45B-7634kLqO".ToCharArray())})
+}
+
+1..1535 | % -PipelineVariable NAME { getName(Get-Random -Minimum 5 -Maximum 17) } | % -PipelineVariable VALUE { getName(Get-Random -Minimum 12 -Maximum 100) } | % {New-Item -Name $NAME -Value $VALUE -ItemType "file"}
+
+dir .\ -*;
+
+Get-ChildItem .\ | where { $_.Name -match "\d"};
