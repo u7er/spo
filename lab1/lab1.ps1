70bef165b5e4c20c3abb92d9f7a24cbe22a84862
Pavel.Manakov <james.swift@yandex.ru>
2019-03-02 01:32:28 +0600

    add first lab

--- lab1/lab1.ps1
+++ lab1/lab1.ps1
@@ -1,9 +1,13 @@
-﻿function getName($len){
+﻿# 1-2
+function getName($len){
     -join (1..$len | % { Get-Random -InputObject ("afgtdUHS45B-7634kLqO".ToCharArray())})
 }
 
-1..1535 | % -PipelineVariable NAME { getName(Get-Random -Minimum 5 -Maximum 17) } | % -PipelineVariable VALUE { getName(Get-Random -Minimum 12 -Maximum 100) } | % {New-Item -Name $NAME -Value $VALUE -ItemType "file"}
-
+1..1535 | % -PipelineVariable NAME { getName(Get-Random -Minimum 5 -Maximum 17) } | % -PipelineVariable VALUE { getName(Get-Random -Minimum 12 -Maximum 100) } | % {New-Item -Name $NAME -Value $VALUE -ItemType "file"} | Out-Null;
+# 3
 dir .\ -*;
 
-Get-ChildItem .\ | where { $_.Name -match "\d"};
+# 4
+$nr = 0;
+Get-ChildItem .\ | Sort-Object -Property lenght | % { Rename-Item $_ -NewName (("{0}" + " " + $_.ToString()) -f $nr++) };
+
