fce058fc2fb2a415e7e476ae4cf43df897c6b591
Pavel Manakov <james.swift@yandex.ru>
2019-03-02 11:57:06 +0700

    Update lab1.ps1

--- lab1/lab1.ps1
+++ lab1/lab1.ps1
@@ -3,7 +3,11 @@ function getName($len){
     -join (1..$len | % { Get-Random -InputObject ("afgtdUHS45B-7634kLqO".ToCharArray())})
 }
 
-1..1535 | % -PipelineVariable NAME { getName(Get-Random -Minimum 5 -Maximum 17) } | % -PipelineVariable VALUE { getName(Get-Random -Minimum 12 -Maximum 100) } | % {New-Item -Name $NAME -Value $VALUE -ItemType "file"} | Out-Null;
+function getSizeFile($len){
+    -join (1..$len | % { Get-Random -InputObject ("1234567890".ToCharArray())})
+}
+
+1..1535 | % -PipelineVariable NAME { getName(Get-Random -Minimum 5 -Maximum 17) } | % -PipelineVariable VALUE { getSizeFile(Get-Random -Minimum 100 -Maximum 1024) } | % {New-Item -Name $NAME -Value $VALUE -ItemType "file"} | Out-Null;
 # 3
 dir .\ -*;
 
